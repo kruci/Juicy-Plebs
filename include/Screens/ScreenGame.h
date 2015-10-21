1b73942ab656bcf2e511b6541bc536a574860550
kruci <rkuci24@gmail.com>
2015-10-21 18:01:34 +0200

    Structure of ScreenGame

--- include/Screens/ScreenGame.h
+++ include/Screens/ScreenGame.h
@@ -1,13 +1,24 @@
+//R.K.
 #ifndef SCREENGAME_H
 #define SCREENGAME_H
 
+#include "global.h"
 
 class ScreenGame
 {
 private:
+    bool cutscene_playing = true;
+    Button *cutscene_button = nullptr;
+
+    bool paused = false;
 public:
     ScreenGame();
     virtual ~ScreenGame();
+
+    bool Set_mission(int mission);
+
+    void Input(ALLEGRO_EVENT &event, float &xscale, float &yscale);
+    void Print();
 };
 
 #endif // SCREENGAME_H
--- src/Screens/ScreenGame.cpp
+++ src/Screens/ScreenGame.cpp
@@ -1,11 +1,51 @@
+//R.K.
 #include "ScreenGame.h"
 
 ScreenGame::ScreenGame()
 {
-    //ctor
+    cutscene_button = new Button("resources/fonts/Calibri.ttf", 1300, global::dHeight -70, 1300 + 120, global::dHeight -70 + 50, "Skip", al_map_rgb(0,0,128));
 }
 
 ScreenGame::~ScreenGame()
 {
-    //dtor
+    if(cutscene_button != nullptr)
+        delete cutscene_button;
+}
+
+void ScreenGame::Input(ALLEGRO_EVENT &event, float &xscale, float &yscale)
+{
+    if(cutscene_playing == true)
+    {
+        cutscene_button->Input(event, xscale, yscale);
+        if(cutscene_button->is_button_clicked() == true)
+        {
+            cutscene_playing = false;
+            cutscene_button->unclick();
+        }
+        return;
+    }
+
+    //mapinput
+    //gui input
+    //check if input to pause
+
+    return;
+}
+
+void ScreenGame::Print()
+{
+    if(cutscene_playing == true)
+    {
+        //cutsecene->Print();
+        cutscene_button->Print();
+        return;
+    }
+
+    //check if paused, and if not, compute
+
+    //print map and entities
+    //print GUI
+    //check if print pause
+
+    return;
 }
